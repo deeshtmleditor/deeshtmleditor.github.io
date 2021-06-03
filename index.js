d1097914c787477374776f552587fc46b1d01065
Dat Dinhquoc <40449395+datdinhquoc@users.noreply.github.com>
2021-06-03 22:54:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -250,7 +250,12 @@ function create_edit_area(){
     
     // Editor.js
     tinymce.init({
-        selector: 'textarea#Edit-Area'
+        selector: 'textarea#Edit-Area',
+        plugins: 'print preview paste importcss searchreplace autolink autosave '+
+        'save directionality code visualblocks visualchars fullscreen image link '+
+        'media template codesample table charmap hr pagebreak nonbreaking anchor '+
+        'toc insertdatetime advlist lists wordcount imagetools textpattern '+
+        'noneditable help charmap quickbars emoticons'
     });
 }
 
